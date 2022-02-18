c37674a6865ef1cab32c75c5c6b11e7412b5b88c
Gulniza <gulnizajanyshova@gmail.com>
2022-02-18 14:33:07 +0300

    hw10

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState([])
+	const [isLoading, setLoading] = useState(false)
+	useEffect(() => {
+		setLoading(true)
+		fetch('https://jsonplaceholder.typicode.com/photos?_limit=9')
+			.then((response) => {
+				if (response.ok) {
+					return response.json()
+				}
+			})
+			.then((data) => setData([...data]))
+		setLoading(false)
+	}, [])
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				{isLoading ? (
+					<p>Loading...</p>
+				) : (
+					<ul className='App-link'>
+						{data.map((el) => (
+							<li className='App-logo-spin' key={el.id}>
+								<h1>Photos</h1>
+								<h2>{el.id}</h2>
+								{el.title}
+								<img
+									className='App-logo'
+									src={el.thumbnailUrl}
+								/>
+							</li>
+						))}
+					</ul>
+				)}
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
